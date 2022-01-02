8c0dbeb8277eba158a71a713bbd212f1687dcf5c
Matt DeZarn <matt.dezarn@gmail.com>
2022-01-02 10:51:44 -0800

    add home page

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
 
 # Create your views here.
+
+class Home(TemplateView):
+    template_name = "home.html"
